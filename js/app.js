97ad66155d89ad4803f142459f26ec71dd8d1115
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-03-13 14:17:12 +1100

    setting up the recipes and tags container and pages for single and tag templates

--- js/app.js
+++ js/app.js
@@ -12,3 +12,8 @@ const navBtnDOM = getElement('.nav-btn');
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links');
 });
+
+const date = getElement('#date');
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
+console.log(currentYear);
